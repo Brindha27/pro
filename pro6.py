f323488e07f1007897a3f9191271e56321d8adbd
Brindha <51919498+Brindha27@users.noreply.github.com>
2019-07-08 19:38:42 +0530

    Update pro6.py

--- pro6.py
+++ pro6.py
@@ -4,6 +4,6 @@ b1=0
 for n in range(len(k)-2):
     for i in range(n+1,len(k)-1):
         for l in range(i+1,len(k)):
-            if k[n]<k[y]<k[z] and n<i<l:
+            if k[n]<k[i]<k[l] and n<i<l:
                 b1=b1+1
 print(b1)
