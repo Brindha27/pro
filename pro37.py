bfb916c365690f391a70065fef6e4e2188ddefd6
Brindha <51919498+Brindha27@users.noreply.github.com>
2019-07-19 11:09:11 +0530

    Create pro37.py

---
+++ pro37.py
@@ -0,0 +1,11 @@
+nn3 = int(input())
+L3 = [int(x) for x in input().split()]
+nn3 = len(L3)
+if nn3==1 :
+    print()
+   
+cnt = 0
+for i in range(1,nn3-1) :
+    if ((L3[i] > L3[i-1]) and (L3[i] > L3[i+1])) or ((L3[i] < L3[i-1]) and (L3[i] < L3[i+1])):
+        cnt += 1
+print(cnt)
