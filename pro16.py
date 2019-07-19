7d0e292b934b2b11303cec2cddf4ef2541bc292d
Brindha <51919498+Brindha27@users.noreply.github.com>
2019-07-19 09:47:20 +0530

    Update pro16.py

--- pro16.py
+++ pro16.py
@@ -1,7 +1,7 @@
-nn=int(input())
+nnn=int(input())
 li=list(map(int,input().split()))
-a=[1]*nn
-for i in range(nn):
+a=[1]*nnn
+for i in range(nnn):
     if(i==0):
         if(li[i]>li[i+1]):
             a[i]=a[i]+a[i+1]
